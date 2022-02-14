1fbaa3d9b40c72dbaab8524ffa5166d427d522d8
Héctor Leonardo Rodríguez <hectorleorodriguez@yahoo.es>
2022-02-14 14:34:55 -0600

    feat: login page

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required])
+  hide = true
+  
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  login(){
+    console.log(this.email.value, this.password.value)
+  }
+
+}
